7e85fead15d05716a0c74210d5016ba55ff62983
Ritik Kumar <ritikkumartulbul83753@gmail.com>
2024-10-02 16:18:06 +0530

    add and modify in bank ctrls and dao

--- Banking-system/src/main/java/Bank/Ctrls/StaffModificationControl.java
+++ Banking-system/src/main/java/Bank/Ctrls/StaffModificationControl.java
@@ -1,11 +1,13 @@
 package Bank.Ctrls;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
 import Bank.Dao.Manager.StaffRegisterDao;
--- Banking-system/src/main/java/Bank/Dao/BankingDao.java
+++ Banking-system/src/main/java/Bank/Dao/BankingDao.java
@@ -1,23 +1,17 @@
 package Bank.Dao;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import Bank.Model.AddInformation;
 
 public class BankingDao {
-
+	
 	//get SQl connection...
-<<<<<<< HEAD
-		public static Connection getConnection() throws ClassNotFoundException, SQLException {
-			String url="jdbc:mysql://localhost:3306/banking_system";
-			String user="root123";
-			String pwd="root123";
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			return DriverManager.getConnection(url,user,pwd);	
-=======
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 		String url="jdbc:mysql://localhost:3306/banking_system";
 		String user="root123";
@@ -64,58 +58,11 @@ public class BankingDao {
 			}catch(Exception e3) {
 				e3.printStackTrace();
 			}
->>>>>>> branch 'Sahdev' of https://github.com/ritikdolly/BankingSystem.git
 		}
-<<<<<<< HEAD
-		
-		public boolean loginUsers(AddInformation login) {
-			Connection con = null;
-			PreparedStatement st =null;
-			boolean flag= false;
-			
-			try {
-				con = BankingDao.getConnection();
-				con.setAutoCommit(false);
-				st =con.prepareStatement("SELECT * FROM login where UserId = ? and password =?");
-				st.setString(1,login.getClientPassword());
-				st.setString(2,login.getClientPassword());
-				ResultSet result = st.executeQuery();
-				if(result.next() ) {
-					login.setServerUserId(result.getString("UserId"));
-					login.setServerPassword(result.getString("password"));
-			        login.setType(result.getString("type"));
-			        login.setUserName(result.getString("UserName"));
-			        flag=true;
-			        System.out.println("in bankingDoa loginUser() if (contn) of try block ");
-				}else {
-					con.rollback();
-					con.commit();
-					flag=false;
-				}
-			}catch(Exception e) {
-				e.printStackTrace();
-				System.out.println("In Catch block");
-				
-			}finally {
-				try {
-					if(con != null)
-						con.close();
-					if(st !=null)
-						st.close();
-					System.out.println("In Finally Try block of bankingDao file");
-				}catch(Exception e3) {
-					e3.printStackTrace();
-				}
-			}
-			return flag;
-		}
-
-=======
 		System.out.println(login.getServerUserId()+" in bank dao file");    		
 		System.out.println(login.getServerPassword()+" in bank dao file");
 		return flag;
 	}
 	
 	
->>>>>>> branch 'Sahdev' of https://github.com/ritikdolly/BankingSystem.git
 }
